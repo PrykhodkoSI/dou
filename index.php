04e6c4ad8677d0bfff04baf04db41d8a8f2b2219
PrykhodkoSI <prykhodko.s.i@gmail.com>
2017-01-25 20:29:14 +0200

    First
    First Commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php //https://dou.ua/lenta/news/ ?>
+
+
+<?php
+$content = file_get_contents("https://dou.ua/lenta/news/");
+$regularexp = '/<article[\s\S]*?<h2[\s\S]*?>[\s\S]*?href="(.*)">\s+(.*)\s+<[\s\S]*?<\/h2>[\s\S]*?<p[\s\S]*?>\s+([\s\S]*?)</';
+preg_match_all($regularexp, $content, $match);
+
+//print_r($match);
+$count = count($match[1]);
+for($i=0; $i<$count; $i++ )
+{
+    echo "<a href='{$match[1][$i]}'><h1>{$match[2][$i]}</h1></a>";
+
+    echo "<p>{$match[3][$i]}</p>";
+}
+
+//print "<pre>";
+//print_r($match);
+//print "</pre>";
+//echo "<p>$match[1]</p>";
+?>
